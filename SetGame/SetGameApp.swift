8d5dd7b71e9e2c724407d20dd6d3344ad13d9bd4
Anton Kinstler <antonkinstler@gmail.com>
2021-12-22 20:49:47 +0500

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Anton Kinstler on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
